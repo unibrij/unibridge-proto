e7f7afa916ef9a7405050f8dd091c45883ca32ce
unibrij <ceo@unibrij.io>
2025-10-30 19:57:15 -0400

    Add files via upload

---
+++ api/_lib/cors.js
@@ -0,0 +1,6 @@
+// api/_lib/cors.js
+export function applyCors(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+}
